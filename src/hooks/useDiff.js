92601d27db38d6506a0f824c7cb31e3906ee1f44
sosukesuzuki <aosukeke@gmail.com>
2021-09-23 23:24:26 +0900

    Calculate diffs in worker

--- src/hooks/useDiff.js
+++ src/hooks/useDiff.js
@@ -1,8 +1,24 @@
-import { useMemo } from "preact-hooks";
-import * as Diff from "diff";
+import { useEffect, useState } from "preact-hooks";
+import workerize from "workerize";
+
+const worker = workerize(
+  `
+  export async function lineDiffs(originalText, changedText) {
+    const { diffLines } = await import("https://cdn.skypack.dev/diff@5.0.0");
+    return diffLines(originalText, changedText, {
+      newlineIsToken: true,
+    });
+  }
+`
+);
 
 export function useDiffs(originalText, changedText) {
-  return useMemo(() => {
-    return Diff.diffLines(originalText, changedText, { newlineIsToken: true });
+  const [diffs, setDiffs] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const result = await worker.lineDiffs(originalText, changedText);
+      setDiffs(result);
+    })();
   }, [originalText, changedText]);
+  return diffs;
 }
